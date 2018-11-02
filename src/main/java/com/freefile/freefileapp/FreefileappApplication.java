75ee212172446015ad5da1ecb2df8685dab9e80e
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-11-02 18:41:12 -0500

    First Commit

---
+++ src/main/java/com/freefile/freefileapp/FreefileappApplication.java
@@ -0,0 +1,12 @@
+package com.freefile.freefileapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreefileappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreefileappApplication.class, args);
+    }
+}

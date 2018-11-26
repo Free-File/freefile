924d58b56e51bb4a5e8b3bbbaf4a4c9fe0fb5edf
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-11-26 12:16:03 -0600

    Bug fixes, small styling

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,20 @@
+
+// Smooth Scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-    e.preventDefault();
+        e.preventDefault();
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
+});
+
+
+
+$("header .nav a[href!=#]").each(function(){
+    $($(this).attr("href"))                             // Fixes issue where navigating to page
+        .css("padding-top", "40px")                     // section would hide top part
+        .prev()                                         // of section behind navbar.
+        .css("margin-bottom", "-40px");
 });
